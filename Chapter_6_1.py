84d202473ae464402df2f14ace90983854287630
kevincao91 <35439861+kevincao91@users.noreply.github.com>
2018-04-18 12:36:52 +0800

    Chapter_7_1 finished
    Chapter_7_1 finished

--- Chapter_6_1.py
+++ Chapter_6_1.py
@@ -92,7 +92,7 @@ plt.imshow(resultImage), plt.show()
 MIN_MATCH_COUNT = 10
 
 img1 = cv2.imread('music_logo.png', cv2.IMREAD_GRAYSCALE)
-img2 = cv2.imread('music_pic2.png', cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread('music_pic3.png', cv2.IMREAD_GRAYSCALE)
 
 # Initiate SIFT detector
 sift = cv2.xfeatures2d.SIFT_create()
@@ -115,6 +115,7 @@ for m, n in matches:
     if m.distance < 0.7*n.distance:
         good.append(m)
 
+print(len(good))
 if len(good) > MIN_MATCH_COUNT:
     src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
     dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
@@ -139,7 +140,7 @@ draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
 
 img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
 
-plt.imshow(img3, 'gray'),plt.show()
+plt.imshow(img3, 'gray'), plt.show()
 
 
 
